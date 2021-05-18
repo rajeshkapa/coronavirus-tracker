0cfb2bd667dfce4cb9d721aad0444b1cbfad1afe
rkapa <rkhv3@mail.umkc.edu>
2021-05-18 10:07:21 -0500

    First commit

---
+++ src/test/java/com/rajagrit/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajagrit.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
